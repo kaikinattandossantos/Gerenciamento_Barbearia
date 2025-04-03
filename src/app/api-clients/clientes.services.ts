98e944ba70704b3f486ed810c2ef5152a00b507d
kaiki nattan dos santos guedes <kaikinattansg@gmail.com>
2025-04-03 11:20:11 -0300

    métodos https

---
+++ src/app/api-clients/clientes.services.ts
@@ -0,0 +1,31 @@
+export interface SaveClientesRequest{
+  name: string
+  email: string
+  phone: string
+}
+export interface SaveClientesResponse{
+  name: string
+  email: string
+  phone: string
+}
+export interface UpdateClientesRequest{
+  name: string
+  email: string
+  phone: string
+}
+export interface UpdateClientesResponse{
+  name: string
+  email: string
+  phone: string
+}
+export interface ListClientesResponse{
+  name: string
+  email: string
+  phone: string
+}
+export interface DetailClientesResponse{
+  name: string
+  email: string
+  phone: string
+}
+
